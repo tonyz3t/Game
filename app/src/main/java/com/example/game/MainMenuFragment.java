e9601610060b3fc2754885c6ee09cbda61f9ac16
tonyz3t <dhanjal.n3@gmail.com>
2020-05-13 16:52:14 -0700

    Added a play button to our main menu

--- app/src/main/java/com/example/game/MainMenuFragment.java
+++ app/src/main/java/com/example/game/MainMenuFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MainMenuFragment extends Fragment {
+
+    private Button mPlayButton;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,6 +24,16 @@ public class MainMenuFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.main_menu_fragment, container, false);
+
+        //Play Button
+        mPlayButton = (Button) v.findViewById(R.id.play_button);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Send the User to our level one activity (to be created)
+
+            }
+        });
 
         return v;
     }
